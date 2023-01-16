0e92b1be673b17dd2da2a2240bc285e6da248ba4
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-16 21:51:59 +0800

    Update Introdution.java

--- Introdution.java
+++ Introdution.java
@@ -75,4 +75,21 @@ public class Main {
 		System.out.println(message3);
 		
 	}	
+
+	public static void arrays(String[] args) {
+		int[] numbers = new int[5];
+		numbers[0]=1;
+		numbers[1]=2;
+		System.out.println(Arrays.toString(numbers));
+		//[1, 2, 0, 0, 0] 
+		
+		int[] numbers2 = {2,3,5,1,4};
+		System.out.println(Arrays.toString(numbers2));
+		//[2, 3, 5, 1, 4]
+		System.out.println(numbers2.length);
+		//5
+		Arrays.sort(numbers2);
+		System.out.println(Arrays.toString(numbers2));
+		//[1, 2, 3, 4, 5]
+	}
 }
