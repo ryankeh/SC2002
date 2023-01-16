4d75c1b86d37e95467035eba4cc5aedf0ef17038
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-16 18:08:33 +0800

    Create Introdution.java

---
+++ Introdution.java
@@ -0,0 +1,14 @@
+package com.pack;
+
+public class Main {
+	public static void main(String[] args) {
+		byte age = 30;
+		long viewCount = 3_123_456_789L;
+		float price = 10.99F;
+		//need put L or F behind numbers to allow engine to read it as long/float values
+		char letter = 'A';
+		//for strings use double quotation marks
+		boolean isEligbible = false;
+		System.out.println(age);
+	}
+}
