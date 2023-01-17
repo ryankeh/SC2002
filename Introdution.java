c77dd9496fb31a32cb0c4d45f1000270b0c0b659
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-17 11:25:31 +0800

    Update Introdution.java

--- Introdution.java
+++ Introdution.java
@@ -130,5 +130,29 @@ public class Main {
 		double y2 = Double.parseDouble(x2)+2;
 		System.out.println(y2);
 	}
+	
+	public static void math(String[] args) {
+		int result = Math.round(1.1F);
+		System.out.println(result);
+		int result2 = (int)Math.ceil(1.1F);
+		//ceil function returns double
+		System.out.println(result2);
+		int result3 = (int)Math.floor(1.1F);
+		System.out.println(result3);
+		// 1 2 1
+		
+		int result4 = Math.max(1, 2);
+		System.out.println(result4);
+		int result5 = Math.min(1, 2);
+		System.out.println(result5);
+		// 2 1
+		
+		double result6 = Math.random();
+		System.out.println(result6);
+		// gives random number from 0 to 1
+		int result7 = (int)Math.round(Math.random()*100);
+		System.out.println(result7);
+		// gives random integer from 0 to 100
+	}
 
 }
