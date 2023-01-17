c8e6423951f9c2e4cb1993f520922cff5e446ddb
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-17 12:07:51 +0800

    Update and rename Introduction (Control Flow).java to Introduction2 (Control Flow).java

--- Introduction2 (Control Flow).java
+++ Introduction2 (Control Flow).java
@@ -12,6 +12,18 @@ public class Main {
 		boolean isEligible = (hasHighIncome||hasGoodCredit)&&!hasCriminalRecord;
 		System.out.println(isEligible);
 	}
+	
+	public static void ifStatement(String[] args) {
+		int temp=32;
+		if(temp>30) {
+			System.out.println("It's a hot day");
+			System.out.println("Drink Water");			
+		} else if (temp>20&&temp<=30) {
+			System.out.println("Beautiful day");
+		} else {
+			System.out.println("Cold day");
+		}
+	}
   
   
 }
