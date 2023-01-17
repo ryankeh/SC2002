3f6feb536f865710fd61edde8bb28c3c8bb3b13e
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-17 12:16:10 +0800

    Update Introduction2 (Control Flow).java

--- Introduction2 (Control Flow).java
+++ Introduction2 (Control Flow).java
@@ -35,6 +35,21 @@ public class Main {
 		System.out.println(className);
 		//First
 	}
+	
+	public static void switchStatement(String[] args) {
+		String role = "admin";
+		switch(role) {
+			case "admin":
+				System.out.println("You're an admin");
+				break;
+			case "moderator":
+				System.out.println("You're a moderator");
+				break;
+			default:
+				System.out.println("You're an guest");
+		}
+		
+	}
   
   
 }
