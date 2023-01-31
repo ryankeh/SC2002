5ed2a02fe53bec4be45ab1f70969819b816046af
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-31 16:29:36 +0800

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -10,13 +10,36 @@ class Main {
 public class Rectangle{
     //instance variables
     private double width;
-    private double length;
+    private double height;
     //instance methods
+    public Rectangle(){
+        this(1.0,1.0);
+    }
+    public Rectangle(double w, double h){
+        width = w;
+        height = h
+    }
+    
+    public void setWidth( double w ) { width = w ; }
+    public void setHeight( double h ) { height = h ; }
+    public double getWidth() { return width ; }
+    public double getHeight() { return height ; }
+    
     public double findArea(){
         return width*height;
     }
     public double findPerimeter(){
         return (width+height)*2;
+    }
+    public Rectangle copyObjects() {
+        Rectangle rect = new Rectangle( width , height );
+        return rect;
+    }
+    public void print() {
+        System.out.println("The area of rectangle is "
+        + this.findArea() );
+        System.out.println("The perimeter of rectangle is "
+        + this.findPerimeter() );
     }
 }
 
@@ -25,5 +48,13 @@ public class RectangleApp {
         Rectangle rect = new Rectangle();
         System.out.println("The area of rectangle is " + rect.findArea() );
         System.out.println("The perimeter of rectangle is " + rect.findPerimeter() );
+    }
+}
+
+public class CopyObjectsApp {
+    public static void main(String[] args){
+        Rectangle rect1, rect2;
+        rect1 = new Rectangle( 10.0 , 20.0 );
+        rect2 = rect1.copyObjects();
     }
 }
