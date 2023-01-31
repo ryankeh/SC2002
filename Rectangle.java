2211eb24ecbbe43dfc8066b867115a84a6ac0fb4
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-31 17:01:46 +0800

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -11,26 +11,53 @@ public class Rectangle{
     //instance variables
     private double width;
     private double height;
-    //instance methods
+    //class constants
+    public static final double MAX_WIDTH = 100;
+    public static final double MAX_HEIGHT = 100;
+    //static variable
+    private static int numRectangles=0;
+    
+    //class methods
     public Rectangle(){
-        this(1.0,1.0);
+        width = 1.0;
+        height = 1.0;
+        numRectangles++;
     }
     public Rectangle(double w, double h){
         width = w;
         height = h
+        numRectangles++;
     }
     
-    public void setWidth( double w ) { width = w ; }
-    public void setHeight( double h ) { height = h ; }
+    public void setWidth( double w ) {
+        if ( w >= MAX_WIDTH )
+            width = MAX_WIDTH ;
+        else
+            width = w ;
+    } 
+
+    public void setHeight( double h ) {
+        if ( h >= MAX_HEIGHT )
+            height = MAX_HEIGHT ;
+        else
+            height = h ;
+    }
+    
+    //instance methods
     public double getWidth() { return width ; }
     public double getHeight() { return height ; }
     
-    public double findArea(){
-        return width*height;
+    public double findArea(){ return width*height; }
+    public double findPerimeter(){ return (width+height)*2; }
+    
+    //class methods
+    public static void setNumRectangles(int number) {
+        numRectangles = number ;
     }
-    public double findPerimeter(){
-        return (width+height)*2;
+    public static int getNumRectangles() {
+        return numRectangles ;
     }
+
     public Rectangle copyObjects() {
         Rectangle rect = new Rectangle( width , height );
         return rect;
@@ -56,5 +83,26 @@ public class CopyObjectsApp {
         Rectangle rect1, rect2;
         rect1 = new Rectangle( 10.0 , 20.0 );
         rect2 = rect1.copyObjects();
+    }
+}
+
+public class UsingStaticApp {
+    public static void main( String[] args ) {
+        Rectangle rect1 = new Rectangle( 5.0 , 10.0 );
+        print(rect1);
+        rect1.setWidth( 40.0 );
+        rect1.setHeight( 10.0 );
+        print(rect1);
+        Rectangle rect2 = new Rectangle( 50.0 , 100.0 );
+        System.out.println( "Number of rectangles = " +
+        Rectangle.getNumRectangles());
+    }
+    public static void print( Rectangle r ){
+        System.out.println( "Width = " + r.getWidth()
+        + "Height = " + r.getHeight() );
+        System.out.println( "The area of rectangle is "
+        + r.findArea() );
+        System.out.println( "The perimeter of rectangle is "
+        + r.findPerimeter() );
     }
 }
